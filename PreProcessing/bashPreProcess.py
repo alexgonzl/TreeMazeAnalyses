85b4ad0c244f054f0db173608419fc8bc82e738c
Alex <alexgb2010@gmail.com>
2018-11-20 15:24:53 -0800

    edits to make sbatch processing work, basically had to create job files to feed into sbatch

--- PreProcessing/bashPreProcess.py
+++ PreProcessing/bashPreProcess.py
@@ -7,25 +7,23 @@ def mkdir_p(dir):
     if not os.path.exists(dir):
         os.mkdir(dir)
 
+
 job_directory = "%s/.job" %os.getcwd()
+mkdir_p(job_directory)
 
-table = "PreProcessingTable_Li_11_13_2018.json"
+table = "PreProcessingTable_Ne_11_19_2018.json"
 
-for t in range(1,45):
+for t in range(2,79):
 
-    job_file = os.path.join(job_directory,"%s.job" %t)
+    job_file = os.path.join(job_directory,"t%s.job" %t)
 
-    with open(job_file) as fh:
+    with open(job_file,"w+") as fh:
         fh.writelines("#!/bin/bash\n")
+        fh.writelines("ml python/3.6.1\n")
         fh.writelines("#SBATCH --job-name=%s.job\n" % t)
-        fh.writelines("#SBATCH --output=.out/%s.out\n" % t)
         fh.writelines("#SBATCH --error=.out/%s.err\n" % t)
         fh.writelines("#SBATCH --time=01:00:00\n")
-        fh.writelines("#SBATCH --mem=8000\n")
-        fh.writelines("#SBATCH --cpus-per-task=4\n")
-        fh.writelines("#SBATCH --partition=giocomo\n")
         fh.writelines("#SBATCH --mail-type=ALL\n")
-        fh.writelines("#SBATCH --mail-user=alexg8@stanford.edu\n")
-        fh.writelines("python3 bashPreProcess.py -t %s -f %s\n" % (t,table))
+        fh.writelines("python3 pyClusterBatch_Session.py -t %s -f %s\n" % (t,table))
 
-    os.system("sbatch %s" %job_file)
+    os.system("sbatch --partition=giocomo --mem=8000 --cpus-per-task=4 --mail-user=alexg8@stanford.edu %s" %job_file)
