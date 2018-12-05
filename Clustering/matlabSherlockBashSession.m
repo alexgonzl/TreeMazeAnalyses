06f5384c99640ed7cc0199f8bac5e9ff12544176
Alex Gonzalez <alexgb2010@gmail.com>
2018-12-05 13:14:00 -0800

    matlab task manager script

--- Clustering/matlabSherlockBashSession.m
+++ Clustering/matlabSherlockBashSession.m
@@ -1,13 +1,29 @@
-function matlabSherlockBashSession(task, table)
+function matlabSherlockBashSession(task, tableFile)
 
-fullTablePath = fullfile(pwd,'TasksDir',table);
+fullTablePath = fullfile(pwd,'TasksDir',tableFile);
 if exists(fullTablePath)
     TaskTable = jsondecode(fileread(fullTablePath));
 else
     error('Task table not found');
+    quit();
 end
 
+taskID = strcat('x',num2str(task));
+session = TaskTable.(taskID).session_name;
+nFiles = TaskTable.(taskID).nFiles;
 
-
-
-
+for f =1:nFiles
+    fID = strcat('x',num2str(f));
+    fInfo =  TaskTable.(taskID).Files.(fID);
+    type = fInfo.type;
+    fn = fInfo.filenames;
+    sp = fInfo.sp;
+    
+    if strcmp(type,'KiloSortCluster')
+        try
+            KiloSort_Master(fn,sp);      
+        catch
+            disp('Error Running KiloSort');
+        end
+    end
+end
