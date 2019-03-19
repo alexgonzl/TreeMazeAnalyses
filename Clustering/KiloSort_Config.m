60f424781b67225e7272060bb76d42a6228b6f8b
Alex Gonzalez <alexgb2010@gmail.com>
2019-03-19 13:22:48 -0700

    mod KS preprocessing to make it work with our data

--- Clustering/KiloSort_Config.m
+++ Clustering/KiloSort_Config.m
@@ -19,7 +19,7 @@ ops.nNeighPC            = 4;           % visualization only (Phy): number of cha
 ops.nNeigh              = 16;           % visualization only (Phy): number of neighboring templates to retain projections of (16)		
 		
 % options for channel whitening		
-ops.whitening           = 'full'; % type of whitening (default 'full', for 'noSpikes' set options for spike detection below)		
+ops.whitening           = 'none'; % type of whitening (default 'full', for 'noSpikes' set options for spike detection below)		
 ops.nSkipCov            = 1; % compute whitening matrix from every N-th batch (1)		
 ops.whiteningRange      = 4; % how many channels to whiten together (Inf for whole probe whitening, should be fine if Nchan<=32)		
 		
@@ -52,7 +52,7 @@ ops.mergeT           = .1;           % upper threshold for merging (.1)
 ops.splitT           = .1;           % lower threshold for splitting (.1)		
 		
 % options for initializing spikes from data		
-ops.initialize      = 'fromData'; %'fromData' or 'no'		
+ops.initialize      = 'no'; %'fromData' or 'no'		
 ops.spkTh           = -4;      % spike threshold in standard deviations (4)		
 ops.loc_range       = [3  1];  % ranges to detect peaks; plus/minus in time and channel ([3 1])		
 ops.long_range      = [30  6]; % ranges to detect isolated peaks ([30 6])		
@@ -61,8 +61,8 @@ ops.crit            = .65;     % upper criterion for discarding spike repeates (
 ops.nFiltMax        = 10000;   % maximum "unique" spikes to consider (10000)		
 		
 % load predefined principal components (visualization only (Phy): used for features)		
-dd                  = load('PCspikes2.mat'); % you might want to recompute this from your own data		
-ops.wPCA            = dd.Wi(:,1:7);   % PCs 		
+dd                  = load('PCspikes.mat'); % you might want to recompute this from your own data		
+ops.wPCA            = dd.Wi(:,1:3);   % PCs 		
 		
 % options for posthoc merges (under construction)		
 ops.fracse  = 0.1; % binning step along discriminant axis for posthoc merges (in units of sd)		
