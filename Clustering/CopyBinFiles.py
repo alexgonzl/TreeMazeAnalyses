81dcfa4901946c3e70e524ab9fbb9867fae567a7
Alex Gonzalez <alexgb2010@gmail.com>
2019-05-13 13:20:26 -0700

    exception handling

--- Clustering/CopyBinFiles.py
+++ Clustering/CopyBinFiles.py
@@ -1,7 +1,7 @@
 import os, json,sys, time, datetime, filecmp
 import numpy as np
 from pathlib import Path
-from shutil import copy
+from shutil import copy2
 
 # PATCH! 4/25/19
 # copy of tetrode binaries to clustering folders. this is required to run the current version of phy
@@ -13,8 +13,8 @@ from shutil import copy
 job_directory = Path("./.job")
 job_directory.mkdir(parents=True, exist_ok=True)
 
-ID = 'Li'
-date = '3_13_2019'
+ID = 'Ne'
+date = '5_10_2019'
 
 table = "Clustering_{}_{}.json".format(ID,date)
 TasksDir = Path.cwd()/'TasksDir'
@@ -31,11 +31,16 @@ for t in [40]:#np.arange(1,nJobs+1):
     nFiles = len(session)
     print()
     for f in np.arange(1,nFiles+1):
-        fn = 'tt_{}.bin'.format(f)
-        f1=session[str(f)]['filenames']
-        f2=str(Path(session[str(f)]['sp'],fn))
-        if not filecmp.cmp(f1,f2):
-            copy(f1,f2)
-            print('Copy completed for file {}'.format(f1))
-        else:
-            print('File {} already exists at destination. '.format(f1))
+        try:
+            fn = 'tt_{}.bin'.format(f)
+            f1=session[str(f)]['filenames']
+            sp=session[str(f)]['sp']
+            f2=str(Path(sp,fn))
+            if not filecmp.cmp(f1,f2):
+                copy2(f1,sp)
+                print('Copy completed for file {}'.format(f1))
+            else:
+                print('File {} already exists at destination. '.format(f1))
+        except:
+            print('Error copying {}'.format(f1))
+            print ("Error", sys.exc_info()[0],sys.exc_info()[1],sys.exc_info()[2].tb_lineno)
