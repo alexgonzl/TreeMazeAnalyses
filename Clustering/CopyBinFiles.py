cb36c14008656c9891263f18e908fa371e45f31e
Alex Gonzalez <alexgb2010@gmail.com>
2019-05-13 13:11:03 -0700

    missing )

--- Clustering/CopyBinFiles.py
+++ Clustering/CopyBinFiles.py
@@ -5,9 +5,9 @@ from shutil import copy
 
 # PATCH! 4/25/19
 # copy of tetrode binaries to clustering folders. this is required to run the current version of phy
-# two solutions: 
+# two solutions:
 # (1) incorporate this copy in the clustering itself
-# (2) make phy read file binaries at other locations other than 
+# (2) make phy read file binaries at other locations other than
 # where the clustering results are.
 
 job_directory = Path("./.job")
@@ -20,13 +20,13 @@ table = "Clustering_{}_{}.json".format(ID,date)
 TasksDir = Path.cwd()/'TasksDir'
 if not TasksDir.exists():
     sys.exit('Task directory not found.')
-    
+
 if (TasksDir/table).exists():
     with open(str(TasksDir/table), 'r') as f:
         task_table = json.load(f)
 
 nJobs = len(task_table)
-for t in np.arange(1,nJobs+1):
+for t in [40]:#np.arange(1,nJobs+1):
     session = task_table[str(t)]['Files']
     nFiles = len(session)
     print()
@@ -34,7 +34,7 @@ for t in np.arange(1,nJobs+1):
         fn = 'tt_{}.bin'.format(f)
         f1=session[str(f)]['filenames']
         f2=str(Path(session[str(f)]['sp'],fn))
-        if not filecmp.cmp(f1,f2:
+        if not filecmp.cmp(f1,f2):
             copy(f1,f2)
             print('Copy completed for file {}'.format(f1))
         else:
