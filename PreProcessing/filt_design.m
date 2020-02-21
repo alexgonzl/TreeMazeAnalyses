32fd01b4a4c85f75457f39bff429c2753509cfb4
Alex Gonzalez <alexgb2010@gmail.com>
2020-02-20 17:53:56 -0800

    minor notebook changes

--- PreProcessing/filt_design.m
+++ PreProcessing/filt_design.m
@@ -18,3 +18,19 @@ fvtool(beq,a,beq2,a,'fs',fs)
 %%
 dlmwrite('filt_a.dat',a,'delimiter',',','precision',12)
 dlmwrite('filt_b.dat',beq2,'delimiter',',','precision',12)
+
+%% theta:
+fs = 32000; % sampling rate
+Ny = fs/2;  % nyquist
+fOrder = 64;
+
+% initial iir filt
+[b0,a] = cheby1(3,0.001,6/Ny,'high');
+
+b_lowpass =  fir1(fOrder, [12]/Ny,kaiser(fOrder+1,8));
+b_highpass=  fir1(fOrder*2, [6]/Ny,'high',blackman(fOrder*2+1));
+
+% form equivalent filter
+beq=conv(conv(b0,b_lowpass),b_highpass);
+beq2=beq;
+beq2(abs(beq)<1e-8)=[];
